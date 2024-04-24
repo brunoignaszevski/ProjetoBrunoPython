3a48443670cd4a6dce82bec41be0530f0e06b19f
brunoignaszevski <brunohenriquewew@outlook.com>
2024-04-24 20:44:46 -0300

    teste2

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-num1 = input("Digite um número: ")
-num2 = input("Digite outro número:")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
 
-soma = int(num1) + int(num2)
+class Janela(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Exemplo PyQt')
+        self.setGeometry(100, 100, 300, 200)
 
-print(soma)
+        self.label = QLabel('Olá, Mundo!', self)
+        self.label.move(100, 50)
 
+        self.botao = QPushButton('Clique Aqui', self)
+        self.botao.move(100, 100)
+        self.botao.clicked.connect(self.mostrar_mensagem)
+
+    def mostrar_mensagem(self):
+        self.label.setText('Botão Clicado')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    janela = Janela()
+    janela.show()
+    sys.exit(app.exec_())
